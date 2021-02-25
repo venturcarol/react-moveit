24be31bb630332d7a6990542d9bba4cb267fd613
Carolina Ventura <carolventura1996@gmail.com>
2021-02-25 13:47:29 +0000

    Context and components

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Profile } from "../components/Profile";
 import { ExperienceBar } from '../components/ExperienceBar';
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
